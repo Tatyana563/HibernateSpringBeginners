fd71467c4f67de03e5ebad0f07705f87de56c701
Tanya.Petrenko <nikolaev.english@gmail.com>
2020-08-12 22:52:18 +0300

    commit changes

--- src/main/java/DeleteInstructorDetailDemo.java
+++ src/main/java/DeleteInstructorDetailDemo.java
@@ -2,6 +2,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 
 public class DeleteInstructorDetailDemo {
     public static void main(String[] args) {
@@ -17,9 +19,18 @@ public class DeleteInstructorDetailDemo {
 
         try {
             session.beginTransaction();
-            int detailId = 2;
-            InstructorDetail detail = session.get(InstructorDetail.class, detailId);
-
+            InstructorDetail detail = (InstructorDetail) session
+                    .createQuery("from InstructorDetail where instructor.email = :email")
+                    .setParameter("email", CreateInstructorDemo.INSTRUCTOR_MAIL)
+                    .uniqueResult();
+            List<Course> list = (List<Course>) session
+                    .createQuery("from Course c where c.instructor.id = :instructorId")
+                    .setParameter("instructorId", detail.getInstructor().getId())
+                    .list();
+            for (Course course : list) {
+                course.setInstructor(null);
+                session.update(course);
+            }
             session.delete(detail);
             session.getTransaction().commit();
         } finally {
